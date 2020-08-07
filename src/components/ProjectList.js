c452f0048a3a0d9957065df3e1160247782e9f7c
Benoit Hubert <benoithubert@gmail.com>
2020-08-08 00:13:44 +0200

    Fetch projects from backend via axios

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import ProjectCard from './ProjectCard';
-import projects from '../data/projects.json';
+import projectsList from '../data/projects.json';
 
-function ProjectList() {
-  return (
-    <div className="album py-5 bg-light">
-      <div className="container">
+class ProjectList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: projectsList,
+    };
+  }
 
-        <div className="row">
-          {
-            projects.map((project) => (
-              <div className="col-md-4" key={project.id}>
-                <ProjectCard project={project} />
-              </div>
-            ))
-          }
+  componentDidMount() {
+    this.getAllProjects();
+  }
+
+  getAllProjects() {
+    axios.get('/api/projects')
+      .then((res) => {
+        this.setState({
+          projects: res.data,
+        });
+      });
+  }
+
+  render() {
+    const { projects } = this.state;
+    return (
+      <div className="album py-5 bg-light">
+        <div className="container">
+          <div className="row">
+            {
+              projects.map((project) => (
+                <div className="col-md-4" key={project.id}>
+                  <ProjectCard project={project} />
+                </div>
+              ))
+            }
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ProjectList;
