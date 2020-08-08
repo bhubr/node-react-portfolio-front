ff97053828a8b733e051c20e423eac5392e45c4c
Benoit Hubert <benoithubert@gmail.com>
2020-08-08 11:02:12 +0200

    Set root element for Modal component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Modal from 'react-modal';
 
 import App from './App';
+
+Modal.setAppElement('#root');
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
